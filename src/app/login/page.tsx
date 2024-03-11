e67bda64e920119405406ae06aed2232123ec358
Edenilson Souza <edenilson.sza@gmail.com>
2024-03-11 15:06:57 -0300

    fix: fix logout no carregamento da página de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 import LoginForm from "@/app/components/forms/login";
 import { ContainerAuth } from "@/app/components/shared/container";
+import useAuthentication from "@/hooks/useAuthentication";
+import { useEffect } from "react";
 
 export default function Login() {
+    const auth = useAuthentication();
+
+    useEffect(() => {
+        auth.logout();
+    }, []);
+
     return (
         <ContainerAuth>
             <LoginForm />
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import SignupForm from "@/app/components/forms/signup";
 import { ContainerAuth } from "@/app/components/shared/container";
+import useAuthentication from "@/hooks/useAuthentication";
+import { useEffect } from "react";
 
 export default function Login() {
+    const auth = useAuthentication();
+
+    useEffect(() => {
+        auth.logout();
+    }, []);
+
     return (
         <ContainerAuth>
             <SignupForm />
